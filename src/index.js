3097a313b3ab45f50c1a9fd8affb1c35055f980b
Shihabudheen US <shihabudheen@dunzo.in>
2021-12-28 15:58:24 +0530

    composition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
+import Composition from 'composition/sendChildParentEvent';
 
-const app = createElement('my-app', { is: MyApp });
+// const app = createElement('my-app', { is: MyApp });
+const app = createElement('composition-site', { is: Composition });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
